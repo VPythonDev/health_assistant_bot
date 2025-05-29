9d6812ac9472c9cd6099e15c6e0fd517b291053c
developer <there is no email>
2025-05-30 02:12:21 +0300

    Changed kb reminder_types

--- src/keyboard_buttons/reminder_types_kb_btns.py
+++ src/keyboard_buttons/reminder_types_kb_btns.py
@@ -1,11 +1,11 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 reminder_types_buttons = [
-    InlineKeyboardButton(text="Одноразовое", callback_data="Single"),
-    InlineKeyboardButton(text="Повторяющееся", callback_data="Repeated")
+    [InlineKeyboardButton(text="Одноразовое", callback_data="Single")],
+    [InlineKeyboardButton(text="Повторяющееся", callback_data="Repeated")]
 ]
 
-reminder_types_kb = InlineKeyboardMarkup(inline_keyboard=[reminder_types_buttons])
+reminder_types_kb = InlineKeyboardMarkup(inline_keyboard=reminder_types_buttons)
 
 repeat_modes_buttons = [
     [InlineKeyboardButton(text="Интервал", callback_data="Interval")],
