aa06a648929af16168e508312007af9e2e1ef40f
developer <there is no email>
2025-05-29 19:00:41 +0300

    Changed kb in anon_edit_profile

--- src/keyboard_buttons/anon_edit_profile_kb_btns.py
+++ src/keyboard_buttons/anon_edit_profile_kb_btns.py
@@ -1,12 +1,12 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
 anonim_edit_profile_buttons = [
-    KeyboardButton(text="Раскрыть профиль"),
-    KeyboardButton(text="🔙Назад")
+    [KeyboardButton(text="Раскрыть профиль")],
+    [KeyboardButton(text="🔙Назад")]
 ]
 
 anonim_edit_profile_kb = ReplyKeyboardMarkup(
-    keyboard=[anonim_edit_profile_buttons],
+    keyboard=anonim_edit_profile_buttons,
     resize_keyboard=True,
     one_time_keyboard=True
 )
